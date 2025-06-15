36b231174466ab9b7d2a9a78c3b1b6d1706890b7
gihasha <dasunfree5@gmail.com>
2025-06-15 23:13:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "GMk3QJAC#lnil4OBeo2o2IzIGvUQcIJPLE9EmqAOdcF9ls9tHTIQ",// session id 💙
+HACKER : process.env.HACKER || "94765775550" //Addmin number 💙
 };
